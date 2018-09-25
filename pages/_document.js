296d23760d279e1c0c3560a8c0715f33aab102f3
Adam Soffer <adam@soffer.space>
2018-09-25 09:38:03 -0400

    Add GTM

--- pages/_document.js
+++ pages/_document.js
@@ -26,8 +26,23 @@ export default class MyDocument extends Document {
             content="width=device-width, initial-scale=1.0"
           />
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WX7D6NC');`
+            }}
+          />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WX7D6NC"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
           <Main />
           <NextScript />
         </body>
